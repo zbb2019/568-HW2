5770df47d9a6ee76b316c74797977f4fd0127d12
Mila Zhao <binbin.zhao@duke.edu>
2023-05-03 18:15:44 -0400

    3. recognize HTTP request type

--- devlog.md
+++ devlog.md
@@ -2,9 +2,11 @@
 ## 1. dummy proxy
 This proxy returns "Hello World" to the client. It is single-threaded. Use the following commands to test it:
 ```bash
-python3 dummy_proxy.py
+python3 proxy.py
 # in another terminal:
 wget http://www.google.com/ -e use_proxy=yes -e http_proxy=127.0.0.1:12345
 ```
 ## 2. multi-thread
 This proxy is multi-threaded now.
+## 3. recognize HTTP request type
+Parse the request header and extract the HTTP verb.
--- proxy.py
+++ proxy.py
@@ -5,10 +5,35 @@ port = 12345
 host = ''
 
 def handle(conn, addr):
-    data = conn.recv(1024)
-    print(data)
-    conn.sendall(b'HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>hello world</h1>')
-    conn.close()
+    msg_buffer = bytearray()
+    header_end = -1
+    while header_end == -1:
+        # read until we find the end of the header
+        data = conn.recv(4096)
+        if not data: # client closed connection, stop reading
+            return
+        msg_buffer.extend(data)
+        header_end = msg_buffer.find(b'\r\n\r\n')
+    header = msg_buffer[:header_end]
+    first_line = header.partition(b'\r\n')[0]
+    fields = first_line.split(b' ')
+    if len(fields) != 3:
+        print('invalid request')
+        conn.close()
+        return
+    verb, path, version = fields
+    if version != b'HTTP/1.1':
+        print(f"unknown HTTP version {version}")
+        conn.close()
+        return
+    if verb == b'GET':
+        print(f"GET request for {path.decode('utf-8')}")
+        conn.sendall(b'HTTP/1.0 200 OK\r\nContent-Type: text/html\r\n\r\n<h1>hello world</h1>')
+        conn.close()
+    else:
+        print(f"unknown request verb {verb}")
+        conn.close()
+        return
 
 # dummy proxy server that returns a fixed response
 def main():
